0dc0d0c541476ed3439846fa563cc6d08092ccd5
konnofuente <konomelifuente@gmail.com>
2022-01-06 04:49:07 +0100

    F

--- src/main/java/com/example/frontend_javaproject/loggedin_admincontroller.java
+++ src/main/java/com/example/frontend_javaproject/loggedin_admincontroller.java
@@ -5,21 +5,26 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class loggedin_admincontroller implements Initializable {
     @FXML private Button btn_return;
+    @FXML private Label label_welcome;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     btn_return.setOnAction(new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent actionEvent) {
-            
+
         }
     });
 
+    }
+    public void setuser_info(String adminname){
+            label_welcome.setText("WELCOME MR "+adminname+" !");
     }
 }
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.frontend_javaproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.frontend_javaproject to javafx.fxml;
-    exports com.example.frontend_javaproject;
-}
