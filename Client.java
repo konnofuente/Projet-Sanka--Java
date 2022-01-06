99e12d58eae14b7567a6ede7421d9c332d9fc219
Abikum <97134473+Abikum@users.noreply.github.com>
2022-01-06 18:50:48 +0100

    Add files via upload

--- Client.java
+++ Client.java
@@ -23,11 +23,47 @@ public class Client {
     }
     public void Test() {
         String choice = "";
-        if (status){
+         int num_t=0;
+        if (status)
+        {
             Scanner clavier = new Scanner(System.in);
             System.out.println("Resultat: Negatif ");
             System.out.println("Voulez vous vous faire Vaccine??");
             System.out.println("OUI ou NON ?? :");
+            choice = clavier.next();
+
+            switch (choice) {
+             case "OUI":
+                System.out.println("yes");
+                break;
+             case "NON":
+                System.out.println("Voulez vous vous faire Vaccine??");
+                break;
+           }
+
+           num_t++;
+
+
+        }
+        else
+            System.out.println("You are Positive. quarantine!!!");
+        }
+
+    public void Checkstatus(String name){
+        String state;
+    
+
+        if(name.Test()==true) {
+            System.out.println("You are positive!! Quarantine.");
+           
+
+        }
+        else{
+            System.out.println("You are negative. Go ahead and take the vaccination");
+            String choice = "";
+            System.out.println("Voulez vous vous faire Vaccine??");
+            System.out.println("OUI ou NON ?? :");
+            Scanner clavier = new Scanner(System.in);
             choice = clavier.next();
 
           switch (choice) {
@@ -38,17 +74,16 @@ public class Client {
                 System.out.println("Voulez vous vous faire Vaccine??");
                 break;
         }
-
-        }
-        else
-            System.out.println("You are Positive. quarantine!!!");
         }
 
-    public void Checkstatus(){
 
     }
-    public static void main(String[] args){
-        Client a = new Client("20K","201s","abi","cmr",67882898);
-        a.Test();
+    public int Num_Client(){
+
+        return Test().num_t;
+        
     }
+    
+
+    
 }
