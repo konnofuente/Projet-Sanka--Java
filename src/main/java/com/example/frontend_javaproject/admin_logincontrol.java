989ca410ddac72f77869c0d25908099eb891a443
konnofuente <konomelifuente@gmail.com>
2022-01-06 21:24:53 +0100

    Update admin_logincontrol.java

--- src/main/java/com/example/frontend_javaproject/admin_logincontrol.java
+++ src/main/java/com/example/frontend_javaproject/admin_logincontrol.java
@@ -7,7 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-
+//
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
