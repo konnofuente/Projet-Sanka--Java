fadea2ccead8c8723f165ad65e8bcb793b95cf16
konnofuente <konomelifuente@gmail.com>
2022-01-06 04:04:58 +0100

    F

--- src/main/java/com/example/frontend_javaproject/loggedin_admincontroller.java
+++ src/main/java/com/example/frontend_javaproject/loggedin_admincontroller.java
@@ -1,4 +1,25 @@
 package com.example.frontend_javaproject;
 
-public class loggedin_admincontroller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class loggedin_admincontroller implements Initializable {
+    @FXML private Button btn_return;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    btn_return.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            
+        }
+    });
+
+    }
 }
