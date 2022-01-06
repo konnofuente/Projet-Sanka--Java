7d017225d76db43c60aa01f83189edae76b24557
monsterkings <97094819+monsterkings@users.noreply.github.com>
2022-01-06 17:42:16 +0100

    Add files via upload

---
+++ Hopital.java
@@ -0,0 +1,14 @@
+public class Hopital {
+    public String Name;
+    public String City;
+    public int Stock_vacc;
+
+    public Hopital(String Name,String City, int Stock_vacc ){
+        this.Name=Name;
+        this.City=City;
+        this.Stock_vacc=Stock_vacc;
+    }
+    public int Supply_vacc(){
+
+    }
+}
