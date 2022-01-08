6ca77f890f347fd1ffa6d69eadaa253e0cb90ee6
konnofuente <konomelifuente@gmail.com>
2022-01-08 13:25:02 +0100

    fuente succeed connect db

--- FRONT END/Connect2/src/JDBC.java
+++ FRONT END/Connect2/src/JDBC.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import java.util.Calendar;
 
-
+public class JDBC {
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection(  "jdbc:mysql://localhost:3306/sanka", "root" , "" );
@@ -13,12 +13,12 @@ import java.util.Calendar;
                 System.out.println(resultSet.getString("clientName"));
             }
             // create a sql date object so we can use it in our INSERT statement
-            Calendar calendar = Calendar.getInstance();
-            java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+           //alendar calendar = Calendar.getInstance();
+          //  java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
 
             // the mysql insert statement
-            String query = " insert into client (idclient, clientName, nationality, phone, status, vac_date)"
-                    + " values (?, ?, ?, ?, ?, ?)";
+            String query = " insert into client (idclient, clientName, nationality, phone, status)"
+                    + " values (?, ?, ?, ?, ?)";
 
             // create the mysql insert preparedstatement
             PreparedStatement preparedStmt = connection.prepareStatement(query);
@@ -27,7 +27,7 @@ import java.util.Calendar;
             preparedStmt.setString  (3, "Gabon");
             preparedStmt.setInt(4, 690779455);
             preparedStmt.setBoolean    (5, true);
-            preparedStmt.setDate   (6, startDate);
+            // preparedStmt.setDate   (6, startDate);
 
             // execute the preparedstatement
             preparedStmt.execute();
--- FRONT END/src/main/java/com/example/frontend_javaproject/servicepagecontroller.java
+++ FRONT END/src/main/java/com/example/frontend_javaproject/servicepagecontroller.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class servicepagecontroller implements Initializable {
-   @FXML private Button btn_return;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
