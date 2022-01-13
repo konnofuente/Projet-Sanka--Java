cdcfe591bf1c3b3ae9841e4b7e185760cf78e6f6
monsterkings <97094819+monsterkings@users.noreply.github.com>
2022-01-13 17:00:44 +0100

    Add files via upload

--- BACKEND/1-SALOMON/BarCode/src/BARCODE/Barcode_Image.java
+++ BACKEND/1-SALOMON/BarCode/src/BARCODE/Barcode_Image.java
@@ -1,5 +1,12 @@
 package BARCODE;
 
 public class Barcode_Image {
+	public static void createImage(String image_name,String myString)  {
+		try {
+		Code128Bean code128 = new Code128Bean();
+		code128.setHeight(15f);
+		code128.setModuleWidth(0.3);
+		code128.setQuietZone(10);
+		code128.doQuietZone(true);
 
 }
